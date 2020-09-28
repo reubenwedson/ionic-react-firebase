795fe9df3d2ee8e14ce49d6de709d3846e772a59
reubenwedson <reubenwedson@gmail.com>
2020-09-28 14:40:04 +0300

    Add list in the main page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import {IonItem, IonList, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonText} from '@ionic/react';
 import React from 'react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
@@ -12,7 +12,17 @@ const Home: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent className="ion-padding">
-         Home page
+         <IonList>
+             {Array(10)
+                 .fill(0)
+                 .map((_, i) =>(
+                     <IonItem key={i}>
+                         <IonText>
+                             List Item {i}
+                         </IonText>
+                     </IonItem>
+                 ))}
+         </IonList>
       </IonContent>
     </IonPage>
   );
