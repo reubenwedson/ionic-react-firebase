88bfaa7d63ad18ab001215d31c85c4680c24aebe
reubenwedson <reubenwedson@gmail.com>
2020-09-30 10:17:17 +0300

    Add android capacitor

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,32 @@
-import {IonItem, IonList, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonText} from '@ionic/react';
+import {
+    IonItem,
+    IonList,
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonTitle,
+    IonToolbar,
+    IonText,
+    IonAvatar,
+    IonLabel, IonItemSliding, IonItemOptions, IonItemOption
+} from '@ionic/react';
 import React from 'react';
-import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
+const arr = [
+    {
+        name: 'Finn',
+        desc: 'desssdf fdsjvfs fsk fsk s skfksfc'
+    },
+    {
+        name: 'Han',
+        desc: 'Loremmmdsds sdnsdsm sdsmms'
+    },
+    {
+        name: 'Ruenne',
+        desc: 'Dsjd dsf sdk s sdfs dddddf desc'
+    }
+]
 const Home: React.FC = () => {
   return (
     <IonPage>
@@ -13,14 +37,24 @@ const Home: React.FC = () => {
       </IonHeader>
       <IonContent className="ion-padding">
          <IonList>
-             {Array(10)
-                 .fill(0)
-                 .map((_, i) =>(
-                     <IonItem key={i}>
-                         <IonText>
-                             List Item {i}
-                         </IonText>
-                     </IonItem>
+             {arr
+                 .map(elem =>(
+                     <IonItemSliding key={elem.name}>
+                         <IonItem>
+                             <IonAvatar>
+                                 <img src="https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y"  alt="avatar"/>
+                             </IonAvatar>
+
+                             <IonLabel className="ion-padding">
+                                 <h2>{elem.name}</h2>
+                                 <p>{elem.desc}</p>
+                             </IonLabel>
+
+                             <IonItemOptions side="end">
+                                 <IonItemOption>View</IonItemOption>
+                             </IonItemOptions>
+                         </IonItem>
+                     </IonItemSliding>
                  ))}
          </IonList>
       </IonContent>
